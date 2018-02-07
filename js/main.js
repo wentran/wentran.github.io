896dd494d35aa89f4a90c932970dcb4bcc693092
wentran <wen.tran1@gmail.com>
2018-02-06 22:48:44 -0500

    testing change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function($) {
   "use strict";
-  $(window).on("load", function() { // makes sure the whole site is loaded
+  $(window).on("load", function() { // makes sure the whole site is loaded first
     //preloader
     $("#status").fadeOut(); // will first fade out the loading animation
     $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
